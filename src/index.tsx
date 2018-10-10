b2c98dcaa77c2127b169d2a52bc6560e474e1ce7
Frithjof Winkelmann <frithjof97@web.de>
2018-10-10 22:08:46 +0200

    Added logger middleware

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,24 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, Middleware } from 'redux';
 import { rootReducer } from './reducers/index';
 import { StoreState, TodoFilter } from './types/index';
 import { Provider } from 'react-redux';
 import { TodoAction } from './actions';
 import App from './components/App';
 
+const logger: Middleware = store => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+
 const store = createStore<StoreState, TodoAction, {}, {}>(rootReducer, {
   todos: [],
   shownTodos: TodoFilter.ALL
-});
+}, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
