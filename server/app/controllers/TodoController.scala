775f0e2e271a4a8bee8910f29cf29911c0e80fdb
Frithjof Winkelmann <frithjof97@web.de>
2018-10-16 16:45:52 +0200

    Change model of todo in backend

--- server/app/controllers/TodoController.scala
+++ server/app/controllers/TodoController.scala
@@ -5,14 +5,15 @@ import play.api._
 import play.api.mvc._
 import com.mohiva.play.silhouette.api._
 import em.auth._
-import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.jdbc.JdbcProfile
 import slick.jdbc.PostgresProfile.api._
 import em.db.TodoTable
 import play.api.libs.json._
+
 import scala.concurrent._
 import em.db.Util._
-import em.model.Todo
+import em.model.{Todo, TodoV}
 import com.mohiva.play.silhouette.api.actions.SecuredRequest
 
 /**
@@ -27,34 +28,22 @@ class TodoController @Inject() (
   extends AbstractController(cc)
   with HasDatabaseConfigProvider[JdbcProfile] {
 
-  def getTodos(search: Option[String], done: Option[Boolean]) = silhouette.SecuredAction.async { implicit request =>
+  def getTodos() = silhouette.SecuredAction.async { implicit request =>
     val q = TodoTable.todo.filter(x => x.loginFk === request.identity.id.get)
-      .optionalFilter(search, (s: String,e: TodoTable) => upper(e.name) like upper("%" + s + "%"))
-      .optionalFilter(done, (d: Boolean,e: TodoTable) => e.done === d)
 
-    returnPaged(q.queryPaged.result,q,db)
-  }
-  
-  def createTodo = silhouette.SecuredAction.async(parse.json[Todo]) { implicit request: SecuredRequest[AuthEnv, Todo] =>
-    db.run(insertAndReturn[Todo, TodoTable](TodoTable.todo, request.body.copy(loginId = request.identity.id.get)))
-      .map(x => Ok(Json.toJson(x)))
-  }
-  
-  def updateTodo(id: Int) = silhouette.SecuredAction.async(parse.json[Todo]) { implicit request: SecuredRequest[AuthEnv, Todo] =>
-    val q = TodoTable.todo.filter(x => x.id === id && x.loginFk === request.identity.id.get)
-    
-    db.run(q.update(request.body.copy(id = Some(id), loginId = request.identity.id.get))).map {
-      case 0 => NotFound
-      case _ => Ok
+    db.run(q.result).map {
+      case todos => Ok(Json.toJson(todos.map(_.toTodoV)))
     }
   }
   
-  def deleteTodo(id: Int) = silhouette.SecuredAction.async { implicit request =>
-    val q = TodoTable.todo.filter(x => x.id === id)
+  def updateTodos = silhouette.SecuredAction.async(parse.json[List[TodoV]]) { implicit request: SecuredRequest[AuthEnv, List[TodoV]] =>
+    //val q = TodoTable.todo.filter(x => x.id === id && x.loginFk === request.identity.id.get)
     
-    db.run(q.delete).map {
+    /*db.run(q.update(request.body.copy(id = Some(id), loginId = request.identity.id.get))).map {
       case 0 => NotFound
       case _ => Ok
-    }
+    }*/
+    // TODO: Implement
+    Future.successful(Ok(Json.toJson(request.request.body)))
   }
 }
--- server/app/em/model/Todo.scala
+++ server/app/em/model/Todo.scala
@@ -9,10 +9,25 @@ case class Todo(id: String, name: String, loginFk: Int,
                 done: Boolean, category: Option[String],
                 timestamp: Timestamp) extends HasCopy[Todo, String] {
   def cpy(i: Option[String]) = this.copy(id = i.getOrElse(this.id))
+
+  def toTodoV = TodoV(id, name, date, priority, done, category, timestamp: Timestamp)
+}
+
+case class TodoV(id: String, name: String,
+                 date: Option[Timestamp], priority: Option[Int],
+                 done: Boolean, category: Option[String],
+                 timestamp: Timestamp) {
+  def toTodo(loginFk: Int) = Todo(id, name, loginFk, date, priority, done, category, timestamp: Timestamp)
 }
 
 object Todo {
   implicit val todoFormat = Json.format[Todo]
+
+  val tupled = (this.apply _).tupled
+}
+
+object TodoV {
+  implicit val todoVFormat = Json.format[TodoV]
 
   val tupled = (this.apply _).tupled
 }
