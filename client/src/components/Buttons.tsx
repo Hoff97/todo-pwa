9ee219d198e1a582fa0b56e07687918d062fb044
Frithjof Winkelmann <frithjof97@web.de>
2018-10-23 18:18:23 +0200

    Pull Suggestion Component into own class

--- client/src/components/Buttons.tsx
+++ client/src/components/Buttons.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
-import * as Autocomplete from 'react-autocomplete';
-import { HTMLProps, CSSProperties } from 'react';
-import { longestPreSuffix } from 'src/util/string';
-import { dateDescrToDate, dExpr } from 'src/util/todo';
 import { CategoryInfo } from 'src/util/category';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
-import * as moment from 'moment';
+import { EnhancedSuggest } from './EnhancedSuggest';
 
 export interface Props {
     value: string;
@@ -20,11 +15,10 @@ export interface Props {
     inputChanged: (str: string) => void;
 }
 
-function renderInput(props: HTMLProps<HTMLInputElement>, undo: () => void, redo: () => void, loggedIn: boolean, login: () => void) {
+function wrapInput(input: JSX.Element, undo: () => void, redo: () => void, login: () => void, loggedIn: boolean) {
     return (
         <div className="input-group">
-            <input {...props} type="text" className="form-control" placeholder="Todo"
-                aria-label="Recipient's username" aria-describedby="button-addon2" />
+            {input}
             <div className="input-group-append">
                 <button className="btn btn-outline-secondary" type="submit"
                     id="button-addon2"><FontAwesomeIcon icon="plus" /></button>
@@ -38,143 +32,12 @@ function renderInput(props: HTMLProps<HTMLInputElement>, undo: () => void, redo:
     );
 }
 
-const menuStyle: CSSProperties = {
-    borderRadius: '0px 0px 5px 5px',
-    border: '1px solid #AAA',
-    boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
-    background: 'rgba(255, 255, 255, 0.9)',
-    fontSize: '90%',
-    position: 'fixed',
-    overflow: 'auto',
-    maxHeight: '50%',
-};
-
-interface ACOption {
-    type: 'prio' | 'category' | 'date' | 'reminder' | 'date-sel';
-    payload: any;
-    label: string;
-}
-
-function prio(prio: number): ACOption {
-    return {
-        type: 'prio',
-        payload: prio,
-        label: '!' + prio
-    };
-}
-
-function date(date: string): ACOption {
-    return {
-        type: 'date',
-        payload: date,
-        label: '@' + date
-    };
-}
-
-function category(category: string, color: string): ACOption {
-    return {
-        type: 'category',
-        payload: color,
-        label: '#' + category
-    };
-}
-
-function reminder(time: string): ACOption {
-    return {
-        type: 'reminder',
-        payload: time,
-        label: 'r:' + time
-    };
-}
-
-function itemsForValue(value: string, categories: CategoryInfo[]) {
-    var items: ACOption[] = [];
-    items = [...items, {
-        type: 'date-sel',
-        payload: '',
-        label: '@'
-    }];
-    items = [...items, ...[5, 4, 3, 2, 1].map(prio)];
-    items = [...items, ...categories.map(cat => category(cat.name, cat.color))];
-    items = [...items,
-    ...['today', 'tomorrow', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'].map(date)];
-    items = [...items, ...['morning', 'noon', 'afternoon', 'evening'].map(rem => reminder(rem))];
-
-    let containsDate = value.match(dExpr) !== null;
-    items = items.filter(item => filterItem(value, item, containsDate));
-    return items;
-}
-
-function filterItem(value: string, item: ACOption, containsDate: boolean) {
-    var ret = longestPreSuffix(value, item.label) > 0;
-    if(item.type === 'reminder') {
-        ret = ret && containsDate;
-    }
-    return ret;
-} 
-
-function appendACOption(value: string, opt: string) {
-    const match = longestPreSuffix(value, opt);
-    return value.substring(0, value.length - match) + opt + ' ';
-}
-
-const renderItem = (value: string, inputChanged: (str: string) => void) => (item: ACOption, isHighlighted: boolean) => {
-    if(item.type !== 'date-sel') {
-        return (
-            <div style={{ background: isHighlighted ? 'lightgray' : 'white' }} className="autocomplete-item"
-                key={item.label}>
-                {item.type === 'prio' &&
-                    <span className={'ml-2 prio ' + 'ml-2 prio' + item.payload}>{item.payload}</span>
-                }
-                {item.type === 'category' &&
-                    <span style={{ color: item.payload }} className="ml-2">{item.label}</span>
-                }
-                {item.type === 'date' &&
-                    <span className="ml-2">{item.payload} ({dateDescrToDate(item.payload).format('DD.MM.')})</span>
-                }
-                {item.type === 'reminder' &&
-                    <span className="ml-2"><FontAwesomeIcon icon="bell"></FontAwesomeIcon> {item.payload}</span>
-                }
-            </div>
-        );
-    } else {
-        return (
-            <div style={{ background: isHighlighted ? 'lightgray' : 'white' }} className="autocomplete-item"
-                    key={item.label}>
-                <DayPicker
-                    onDayClick={handleCompleteDay(value, inputChanged)}/>
-            </div>
-        );
-    }
-};
-
-const handleCompleteDay = (value: string, inputChanged: (str: string) => void) => (day: Date) => {
-    let dayM = moment(day);
-    if(dayM.isAfter(moment()) && dayM.isBefore(moment().add(1, 'years'))) {
-        inputChanged(value + moment(day).format('DD-MM'));
-    } else {
-        inputChanged(value + moment(day).format('DD-MM-YYYY'));
-    }
-};
-
 export function Buttons({ value, addTodo, undo, redo, inputChanged, categories, loggedIn, login }: Props) {
-    const acItems = itemsForValue(value, categories);
     return (
         <div className="mb-2">
             <form onSubmit={e => { e.preventDefault(); addTodo(value); }}>
-                <Autocomplete
-                    getItemValue={(item) => item.label}
-                    items={acItems}
-                    open={acItems.length > 0}
-                    renderItem={renderItem(value, inputChanged)}
-                    wrapperStyle={{ width: '100%' }}
-                    renderInput={props => renderInput(props, undo, redo, loggedIn, login)}
-                    value={value}
-                    onChange={(e) => inputChanged(e.target.value)}
-                    onSelect={(val) => inputChanged(appendACOption(value, val))}
-                    menuStyle={menuStyle}
-                    isItemSelectable={item => item.type !== 'date-sel'}
-                />
+                <EnhancedSuggest value={value} change={inputChanged} categories={categories}
+                    wrapInput={input => wrapInput(input, undo, redo, login, loggedIn)} />
             </form>
         </div>
     );
