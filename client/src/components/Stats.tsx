aaa6e0c150fc4d343a23b648ee9f796b7bac62d4
Frithjof Winkelmann <frithjof97@web.de>
2018-11-07 16:54:41 +0100

    Add chart for todos done by date

--- client/src/components/Stats.tsx
+++ client/src/components/Stats.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
-import { Bar } from 'react-chartjs-2';
+import { Bar, Line } from 'react-chartjs-2';
 import { CategoryInfo } from 'src/util/category';
+import { ChartScales } from 'chart.js';
 
 interface Props {
     openTodos: number;
@@ -9,6 +10,10 @@ interface Props {
     ttD: number;
     categories: CategoryInfo[];
     todosByCategory: number[];
+    todosByDate: {
+        day: string;
+        todos: number;
+    }[];
 }
 
 function prettyDuration(dur: number) {
@@ -37,8 +42,21 @@ function prettyDuration(dur: number) {
     return ret.trim();
 }
 
-function Stats({ openTodos, closedTodos, ttD, categories, todosByCategory }: Props) {
-    const data = {
+const scaleOptions: ChartScales = {
+    yAxes: [{
+        ticks: {
+            beginAtZero: true,
+            callback: function(value, index, values) {
+                if (Math.floor(value) === value) {
+                    return value;
+                }
+            }
+        }
+    }]
+};
+
+function Stats({ openTodos, closedTodos, ttD, categories, todosByCategory, todosByDate }: Props) {
+    const barData = {
         labels: categories.map(x => x.name),
         datasets: [
             {
@@ -52,6 +70,36 @@ function Stats({ openTodos, closedTodos, ttD, categories, todosByCategory }: Pro
             }
         ]
     };
+
+    const L = Line as any
+    
+    const lineData = {
+        labels: todosByDate.map(x => x.day),
+        datasets: [
+          {
+            label: 'Todos done by date',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: todosByDate.map(x => x.todos)
+          }
+        ]
+      };
+
     return (
         <div className="container mt-2">
             <table className="table table-striped table-bordered">
@@ -71,20 +119,15 @@ function Stats({ openTodos, closedTodos, ttD, categories, todosByCategory }: Pro
                 </tbody>
             </table>
             <Bar
-                data={data}
-                width={100}
-                height={50}
+                data={barData}
                 options={{
-                    maintainAspectRatio: false,
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
+                    scales: scaleOptions
                 }}
             />
+            <L data={lineData}
+                options={{
+                    scales: scaleOptions
+                }}/>
         </div>
     );
 }
--- client/src/containers/Stats.tsx
+++ client/src/containers/Stats.tsx
@@ -6,6 +6,8 @@ import Stats from 'src/components/Stats';
 import moment from 'moment';
 import { catInfoFromTodos } from 'src/util/category';
 
+const dayFormat = 'DD.MM.';
+
 export function mapStateToProps(state: StoreState) {
     const open = state.todos.filter(x => !x.done).length;
 
@@ -22,12 +24,22 @@ export function mapStateToProps(state: StoreState) {
             category])
         .sort((x,y) => (y[0] as number) - (x[0] as number))
 
+    const days = [0,-1,-2,-3,-4,-5,-6,-7].reverse()
+        .map(x => moment().add(x, 'days').format(dayFormat))
+        .map(day => {
+            return {
+                day,
+                todos: state.todos.filter(x => x.done && moment(x.timestamp).format(dayFormat) === day).length
+            };
+        });
+
     return {
         openTodos: open,
         closedTodos: state.todos.length - open,
         ttD,
         categories: todosByCategory.map(x => x[1]),
-        todosByCategory: todosByCategory.map(x => x[0])
+        todosByCategory: todosByCategory.map(x => x[0]),
+        todosByDate: days
     }
 }
 
