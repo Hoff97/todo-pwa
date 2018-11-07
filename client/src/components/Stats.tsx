1f6a5468ca2adefc277d5929c437438de908d6a9
Frithjof Winkelmann <frithjof97@web.de>
2018-11-07 17:44:56 +0100

    Styling for charts

--- client/src/components/Stats.tsx
+++ client/src/components/Stats.tsx
@@ -46,7 +46,7 @@ const scaleOptions: ChartScales = {
     yAxes: [{
         ticks: {
             beginAtZero: true,
-            callback: function(value, index, values) {
+            callback: function (value, index, values) {
                 if (Math.floor(value) === value) {
                     return value;
                 }
@@ -72,33 +72,33 @@ function Stats({ openTodos, closedTodos, ttD, categories, todosByCategory, todos
     };
 
     const L = Line as any
-    
+
     const lineData = {
         labels: todosByDate.map(x => x.day),
         datasets: [
-          {
-            label: 'Todos done by date',
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: todosByDate.map(x => x.todos)
-          }
+            {
+                label: 'Todos done by date',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: todosByDate.map(x => x.todos)
+            }
         ]
-      };
+    };
 
     return (
         <div className="container mt-2">
@@ -118,16 +118,22 @@ function Stats({ openTodos, closedTodos, ttD, categories, todosByCategory, todos
                     </tr>
                 </tbody>
             </table>
-            <Bar
-                data={barData}
-                options={{
-                    scales: scaleOptions
-                }}
-            />
-            <L data={lineData}
-                options={{
-                    scales: scaleOptions
-                }}/>
+            <div className="row">
+                <div className="col">
+                    <Bar
+                        data={barData}
+                        options={{
+                            scales: scaleOptions
+                        }}
+                    />
+                </div>
+                <div className="col">
+                    <L data={lineData}
+                        options={{
+                            scales: scaleOptions
+                        }} />
+                </div>
+            </div>
         </div>
     );
 }
