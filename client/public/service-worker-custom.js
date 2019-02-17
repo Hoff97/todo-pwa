7cac1f36b89b1fa3eab9085e3918ded3ad43d55f
Frithjof Winkelmann <frithjof97@web.de>
2019-02-17 18:47:49 +0100

    Change Badge URL

--- client/public/service-worker-custom.js
+++ client/public/service-worker-custom.js
@@ -30,7 +30,7 @@ self.addEventListener('push', function (event) {
                 body: payload.content,
                 icon: '/favicon.ico',
                 actions: actions,
-                badge: '/favicon-small.png',
+                badge: 'https://todo.haskai.de/favicon-small.png',
                 data: { id: payload.data, token: payload.token, notificationId: payload.id }
             });
         }
